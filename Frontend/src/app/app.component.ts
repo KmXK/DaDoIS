82b66535e72138d576420b53121cff1379243e0a
KmXK <guydo.k@yandex.by>
2024-01-31 20:14:18 +0300

    Remove test components data

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TestComponent } from "./test/test.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    template: `
-    <h1>HELLO WORLD</h1>
-
-    <app-test/>
-
-    <router-outlet />
-  `,
+    templateUrl: 'app.component.html',
     styles: [],
-    imports: [RouterOutlet, TestComponent]
+    imports: []
 })
 export class AppComponent {
-  title = 'DaDoIS.Frontend';
 }
